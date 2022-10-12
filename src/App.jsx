2edeb7726935975387086aac09acc4b43226a5ea
Mohamed <nrookman_27@yahoo.com>
2022-10-13 01:39:30 +0200

    implementing close cart modal function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import MyHeader from "./components/Layout/MyHeader";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    const toggleCartHandler = (old) => {
+        setCartIsShown(!cartIsShown);
+    };
+
     return (
         <>
-            <Cart />
-            <MyHeader />
+            {cartIsShown && <Cart onClose={toggleCartHandler} />}
+            <MyHeader onCartClick={toggleCartHandler} />
             <main>
                 <Meals />
             </main>
--- src/components/Layout/MyHeader.jsx
+++ src/components/Layout/MyHeader.jsx
@@ -1,5 +1,3 @@
-import { Fragment } from "react";
-
 import HeaderCartButton from "./HeaderCartButton";
 
 import mealsImage from "../../assets/meals.jpg";
@@ -7,15 +5,15 @@ import classes from "./MyHeader.module.css";
 
 function Header(props) {
     return (
-        <Fragment>
+        <>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onCartClick} />
             </header>
             <div className={classes["main-image"]}>
                 <img src={mealsImage} alt="A table full of delicious food!" />
             </div>
-        </Fragment>
+        </>
     );
 }
 
