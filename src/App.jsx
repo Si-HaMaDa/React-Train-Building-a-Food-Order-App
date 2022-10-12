bfdf9aa471d8079332d70404cee39d048056465c
Mohamed <nrookman_27@yahoo.com>
2022-10-13 00:39:10 +0200

    Start Meals component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import MyHeader from "./components/Layout/MyHeader";
+import Meals from "./components/Meals/Meals";
 
 function App() {
     return (
         <>
             <MyHeader />
+            <main>
+                <Meals />
+            </main>
         </>
     );
 }
