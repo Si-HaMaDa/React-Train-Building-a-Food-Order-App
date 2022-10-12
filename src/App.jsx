83b8987d898fffe247e0f3c71c1b62acd87c83ce
Mohamed <nrookman_27@yahoo.com>
2022-10-13 01:22:20 +0200

    Adding Modal UI component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import MyHeader from "./components/Layout/MyHeader";
 import Meals from "./components/Meals/Meals";
+import Modal from "./components/UI/Modal";
 
 function App() {
     return (
         <>
+            <Modal />
             <MyHeader />
             <main>
                 <Meals />
