1f32933b4a24e1d1c35eca1a42af199d8cbb627b
Mohamed <nrookman_27@yahoo.com>
2022-10-26 21:58:46 +0200

    Add form state and validation

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -1,27 +1,107 @@
+import { useRef, useState } from "react";
 import classes from "./Checkout.module.css";
 
+const notEmpty = (value) => value.trim().length > 0;
+const isFiveChars = (value) => value.trim().length === 5;
+
 function Checkout(props) {
+    const [formInputsState, setFormInputsState] = useState({
+        name: {
+            valid: true,
+            value: "",
+            ref: useRef(),
+            classNames: classes.control,
+            valdation: notEmpty,
+        },
+        street: {
+            valid: true,
+            value: "",
+            ref: useRef(),
+            classNames: classes.control,
+            valdation: notEmpty,
+        },
+        city: {
+            valid: true,
+            value: "",
+            ref: useRef(),
+            classNames: classes.control,
+            valdation: notEmpty,
+        },
+        postal: {
+            valid: true,
+            value: "",
+            ref: useRef(),
+            classNames: classes.control,
+            valdation: isFiveChars,
+        },
+    });
+
     const confirmHandler = (event) => {
         event.preventDefault();
+
+        let newFormState = { ...formInputsState };
+        let formIsValid = true;
+
+        for (let inputName in formInputsState) {
+            newFormState[inputName].value =
+                formInputsState[inputName].ref.current.value;
+
+            let valueIsValid = formInputsState[inputName].valdation(
+                newFormState[inputName].value
+            );
+            newFormState[inputName].valid = valueIsValid;
+
+            newFormState[inputName].classNames = `${classes.control} ${
+                valueIsValid ? "" : classes.invalid
+            }`;
+
+            if (!valueIsValid) formIsValid = false;
+        }
+
+        setFormInputsState(newFormState);
+
+        if (!formIsValid) return;
+
+        console.log("Submit the data!");
     };
 
     return (
         <form className={classes.form} onSubmit={confirmHandler}>
-            <div className={classes.control}>
+            <div className={formInputsState.name.classNames}>
                 <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" />
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    ref={formInputsState.name.ref}
+                />
             </div>
-            <div className={classes.control}>
+            <div className={formInputsState.street.classNames}>
                 <label htmlFor="street">Street</label>
-                <input type="text" id="street" />
+                <input
+                    type="text"
+                    id="street"
+                    name="street"
+                    ref={formInputsState.street.ref}
+                />
             </div>
-            <div className={classes.control}>
+            <div className={formInputsState.postal.classNames}>
                 <label htmlFor="postal">Postal Code</label>
-                <input type="text" id="postal" />
+                <input
+                    type="text"
+                    id="postal"
+                    name="postal"
+                    ref={formInputsState.postal.ref}
+                />
             </div>
-            <div className={classes.control}>
+            <div className={formInputsState.city.classNames}>
                 <label htmlFor="city">City</label>
-                <input type="text" id="city" />
+                <input
+                    type="text"
+                    id="city"
+                    name="city"
+                    ref={formInputsState.city.ref}
+                />
             </div>
             <div className={classes.actions}>
                 <button type="button" onClick={props.onCancel}>
